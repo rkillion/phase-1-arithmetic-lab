4b6bc1d19d54057603f7dd377b0274b98510aa74
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-07-19 08:54:02 -0400

    commit tree 8c2c724cfb8b3c176b11f9155874ee42e53af541

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+let num3 = 20;
+let num4 = 16;
+let random = Math.round((Math.random()*100)+1);
+let mod = num3 % num4;
+let max = Math.max(1,2,3,4,5,20);
 
 // Write your code here
